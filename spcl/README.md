df3d9a022d886b694650186209e4fe66033aafb1
Curtis L. Olson <curtolson@flightgear.org>
2025-09-14 08:22:31 -0500

    Circling back to the code emiter now that I have done work with type checking and validation.  Note that this process iterates through all the steps (tokenizer, parser, type checking, code emiter) as I puzzle my way through the language and features I am creating and the use cases I want to support.

--- spcl/README.md
+++ spcl/README.md
@@ -1,2 +1,35 @@
 # SPCL - Simple Pythonic Control and Logic
 
+This project follows (pretty much mostly?) the standard approach to writing
+parsers and compilers.
+
+1. Tokenizer: Slits the input stream up into atomic tokens. This is the first
+   step that converts the raw input stream into symbols, key words, int vs.
+   float literals, etc.  The tokenizer doesn't care about the order or structure
+   of the tokens.
+
+2. Parser:  I have implemented a recursive descent parser (languages are
+   recursive.)  This step iterates through the token stream, validates the input
+   follows the grammar rules of the language, and builds an abstract syntax tree
+   (AST).  The AST captures and represents the recursive structure of the input
+   program.  Here we resolve order of operations of expression evaluation and
+   separate them out into "binary" (left op right) operations executed in the
+   proper order.  The program can be "executed" in proper order by doing a dept
+   first traversal of the AST.  The parser does not care about the data types of
+   literals and variables, it only ensures the grammar rules are satisfied.
+
+3. Type validator: This step iterates through the abstract syntax Tree (AST) and
+   validates the data types of variables and literals.  Here is where the system
+   catches if a program attempt to multiply an integer by a string or return a
+   floating point number from a function marked as returning a boolean.  Here we
+   can build up the block symbol tables dynamically and determin if a variable
+   is used before it is defined.
+
+   Because iterating through the AST and type checking the code is so parallel
+   to the process of emitting output code, these steps are merged.
+
+4. Emiter: Currently the output target is C++ code.  In some future time frame
+   I'm interested in developing an interpreter so the output would be byte code.
+   This is still a hobby project and I'm puzzling my way through the steps.  In
+   the back of my head as I'm designging and building forwards, I am trying to
+   allow for this to also be a script language.
--- spcl/parser.py
+++ spcl/parser.py
@@ -1,6 +1,5 @@
 # an attempt to remember how to write a recursive descent parser ... wish me luck!
 
-from copy import deepcopy
 import json
 import types
 
@@ -354,162 +353,7 @@ class Parser():
         else:
             print("error")
 
-# Types: int, float (double), string, bool
-# Strict type checking, no implicit int->float type promotion.
 
-from symbols import SymbolTable, FunctionTable
-global_symbols = SymbolTable()
-global_funcs = FunctionTable()
-
-def compare_types(sym, a, b):
-    if a is None or b is None:
-        return None
-    elif a == b:
-        return a
-    else:
-        # print("Type mismatch:", a, b)
-        return None
-
-def resolve_types_expr(sym, expression):
-    # print("expression:", expression)
-    if "INTEGER" in expression:
-        return "int"
-    elif "FLOAT" in expression:
-        return "float"
-    elif "STRING" in expression:
-        return "string"
-    elif "BOOLEAN" in expression:
-        return "bool"
-    elif "op" in expression:
-        op = expression["op"]
-        lineno = expression["lineno"]
-        left = resolve_types_expr(sym, expression["left"])
-        right = resolve_types_expr(sym, expression["right"])
-        result = compare_types(sym, left, right)
-        if result is None:
-            print("Type mismatch in expression line:", lineno, left, op, right)
-        if op in ["EQ", "NE", "GT", "LT", "GTE", "LTE", "NOT"]:
-            result = "bool"
-        return result
-    elif "call" in expression:
-        return resolve_types_call(sym, expression["call"])
-    elif "ID" in expression or "array_deref" in expression:
-        if "array_deref" in expression:
-            id = expression["array_deref"]["name"]
-            lineno = expression["array_deref"]["lineno"]
-            index_type = resolve_types_expr(sym, expression["array_deref"]["expr"])
-            if index_type != "int":
-                print("array index type must resolve to an integer type.")
-        elif "ID" in expression:
-            id = expression["ID"]
-            lineno = expression["lineno"]
-        if sym.check(id):
-            print(id, sym.get_type(id))
-            return sym.get_type(id)
-        else:
-            print("Symbol %s used before definition.  Line %d" % (id, lineno))
-            # print("symbol table:", sym.symbols)
-    elif "list" in expression:
-        result = ""
-        for item in expression["list"]["items"]:
-            tmp = resolve_types_expr(sym, item)
-            print("item:", item, "type:", tmp)
-            if result == "":
-                result = tmp
-            else:
-                if result != tmp:
-                    print("Mismatched types in list on line: %d  %s vs %s" % (expression["list"]["lineno"], result, tmp))
-        return result;
-    else:
-        print("unhandled expression:", expression)
-
-def resolve_types_call(sym, call):
-    calling_params = call["params"]
-    function_params = global_funcs.get_params(call["name"])
-    if type(function_params) is str and function_params == "any":
-        # ok
-        print("calling:", call["name"], "with any parameters ok.")
-    elif len(calling_params) == len(function_params):
-        # print("call:", call)
-        for i in range(len(calling_params)):
-            # print("  params %d" % i, function_params[i])
-            p1 = resolve_types_expr(sym, call["params"][i])
-            p2 = function_params[i]["type"]
-            if p1 != p2:
-                print("Parameter type mismatch in function call on line:", call["lineno"], "%s()" % call["name"], "parameter:", i, p1, "vs", p2)
-                break
-    else:
-        print("Wrong number of parameters in function call on line:", call["lineno"], "%s()" % call["name"], len(function_params), "vs", len(calling_params))
-    return global_funcs.get_type(call["name"])
-
-def resolve_types_statement(sym, statement, function_type):
-    # statements do not propagate a type up the syntax tree
-    if "conditional" in statement:
-        for c in statement["conditional"]:
-            e = c["expression"]
-            result = resolve_types_expr(sym, e)
-            sub_sym = deepcopy(sym)  # variable assignments in this block aren't visible outside.
-            for s in c["statements"]:
-                result = resolve_types_statement(sub_sym, s, function_type)
-    elif "assign" in statement:
-        # print("assign right:", statement["assign"])
-        lhs = statement["assign"]["left"]
-        if "ID" in lhs:
-            lhs_id = lhs["ID"]
-        elif "array_deref" in lhs:
-            lhs_id = lhs["array_deref"]["name"]
-            index_type = resolve_types_expr(sym, lhs["array_deref"]["expr"])
-            if index_type != "int":
-                print("array index type must resolve to an integer type.")
-        right = resolve_types_expr(sym, statement["assign"]["right"])
-        lineno = statement["assign"]["lineno"]
-        if sym.check(lhs_id):
-            lhs_type = sym.get_type(lhs_id)
-            if lhs_type != right:
-                print("Type mismatch in expression line:", lineno, lhs_id, "=", right)
-        else:
-            sym.add(lhs_id, right)
-    elif "call" in statement:
-        resolve_types_call(sym, statement["call"])
-        # calling_params = c["params"]
-        # function_params = global_funcs.get_params(c["name"])
-        # if type(function_params) is str and function_params == "any":
-        #     # ok
-        #     print("calling:", c["name"], "with any parameters ok.")
-        #     pass
-        # elif len(calling_params) == len(function_params):
-        #     print("c:", c)
-        #     for i in range(len(calling_params)):
-        #         p1 = resolve_types_expr(sym, c["params"][i])
-        #         p2 = function_params[i]["type"]
-        #         if p1 != p2:
-        #             print("Parameter type mismatch in function call on line:", c["lineno"], "%s()" % c["name"], "parameter:", i, p1, "vs", p2)
-        #             break
-        # else:
-        #             print("Wrong number of parameters in function call on line:", c["lineno"], "%s()" % c["name"], len(function_params), "vs", len(calling_params))
-    elif "return" in statement:
-        result = resolve_types_expr(sym, statement["return"])
-        if result != function_type:
-            print("function of type: %s returns type: %s" % (function_type, result))
-        else:
-            print("function of type: %s returns the correct type!" % function_type)
-    else:
-        print("unhandled statement:", statement)
-
-def resolve_types(ast):
-    p = ast["program"]
-    for f in p["functions"]:
-        id = f["ID"]
-        return_type = f["TYPE"]
-        sym = SymbolTable()
-        for param in f["parameters"]:
-            key = next(iter(param))
-            sym.add(key, param[key])
-        for statement in f["statements"]:
-            result = resolve_types_statement(sym, statement, return_type)
-        global_funcs.add(id, return_type, f["parameters"])
-    for statement in p["statements"]:
-        result = resolve_types_statement(sym, statement, None)
 
 if __name__ == '__main__':
 
@@ -563,5 +407,3 @@ update(1, 2., True)
     ast = parser.program()
     print("ast:")
     print(json.dumps(ast, indent="  "))
-
-    resolve_types(ast)
